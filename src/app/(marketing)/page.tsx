e7885495361d97ce09a113e4261bfbce4a193e6c
Júnior Nascimento <junior.nascm@gmail.com>
2024-04-21 01:02:57 -0300

    feat: adding auth

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="max-w-[998px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image src="/hero.svg" fill alt="Hero" />
+      </div>
+    </div>
+  );
+}
