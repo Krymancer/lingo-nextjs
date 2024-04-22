ef68dbe98cf2131a82de6d8ac0e6c6158f3d9fe0
Júnior Nascimento <junior.nascm@gmail.com>
2024-04-21 21:28:51 -0300

    feat: adding neon and drizzle

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,7 +11,7 @@ export default function MainLayout({ children }: Props) {
       <MobileHeader />
       <Sidebar clasName="hidden lg:flex"/>
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-        <div className="bg-red-500 h-full">
+        <div className="max-w-[1056px] mx-auto h-full">
         {children}
         </div>
       </main>
--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,9 +1,23 @@
+import FeedWrapper from "@/components/feed-wrapper"
+import StickyWrapper from "@/components/sticky-wrapper"
+import Header from "./header"
+import { User } from "lucide-react"
+import UserProgress from "@/components/user-progress"
+
 export default function Learn() {
   return (
-    <>
-      <main>
-        learn page haha
-      </main>
-    </>
+      <div className="flex flex-row-reverse gap-12 px-6">
+        <StickyWrapper>
+          <UserProgress activeCourse={{title: "Portuguese", imageSrc: "br.svg"}} hearts={5} points={100} hasActiveSubscription={false} />
+        </StickyWrapper>
+        <FeedWrapper>
+          <Header title="Portuguese" />
+          <div className="space-y-4">
+            <div className="h-[700px] bg-blue-500"/>
+            <div className="h-[700px] bg-blue-500"/>
+            <div className="h-[700px] bg-blue-500"/>
+          </div>
+        </FeedWrapper>
+      </div>
   )
 }
